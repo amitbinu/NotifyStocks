e6775ff017f2ba86798da5420424639fdf0c233e
Binu <binua@mcmaster.ca>
2020-06-11 09:20:53 -0400

    adding code to send emails with real time data

--- src/main.py
+++ src/main.py
@@ -1,16 +1,29 @@
 import ConfigParser
 from company import company
+from person import person
 import importlib
-
 sp = importlib.import_module("getStockPrice")
 
 config = ConfigParser.ConfigParser()
 config.read("../apiKeys.properties")
-
 apiKey=config.get("financialmodelingprep", "notifyStocksApiKey")
 
-apple = company("GOOGL")
+companies=["GOOGL","DGLY","SNE"]
 
-url = apple.getUrl(apiKey)
+stockPrices={}
 
-print(sp.getStockPrice(url))
+#testStockPrices={'DGLY': "5.37", 'SNE': "70.51", 'GOOGL': "1464.7"}
+
+for comp in companies:
+    companyObj = company(comp)
+    url = companyObj.getUrl(apiKey)
+    stockPrice = sp.getStockPrice(url)
+    stockPrices[comp] = str(stockPrice)
+
+binu = person("binua@mcmaster.ca","Stock Notifications",stockPrices)
+binu.sendEmail()
+
+#print(sp.getStockPrice(url))
+
+
+
