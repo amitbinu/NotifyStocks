21b0029173654511ee522a85849986420158cbde
Binu <binua@mcmaster.ca>
2020-06-12 08:27:14 -0400

    The main class that calls the notify module every day at specified times

--- src/main.py
+++ src/main.py
@@ -1,29 +1,15 @@
-import ConfigParser
-from company import company
-from person import person
-import importlib
-sp = importlib.import_module("getStockPrice")
+import schedule
+import time
+from notify import notify
 
-config = ConfigParser.ConfigParser()
-config.read("../apiKeys.properties")
-apiKey=config.get("financialmodelingprep", "notifyStocksApiKey")
+def job():
+    notify()
 
-companies=["GOOGL","DGLY","SNE"]
+schedule.every().day.at("09:45").do(job)
+schedule.every().day.at("11:00").do(job)
+schedule.every().day.at("11:30").do(job)
+schedule.every().day.at("12:30").do(job)
 
-stockPrices={}
-
-#testStockPrices={'DGLY': "5.37", 'SNE': "70.51", 'GOOGL': "1464.7"}
-
-for comp in companies:
-    companyObj = company(comp)
-    url = companyObj.getUrl(apiKey)
-    stockPrice = sp.getStockPrice(url)
-    stockPrices[comp] = str(stockPrice)
-
-binu = person("binua@mcmaster.ca","Stock Notifications",stockPrices)
-binu.sendEmail()
-
-#print(sp.getStockPrice(url))
-
-
-
+while True:
+    schedule.run_pending()
+    time.sleep(60)
