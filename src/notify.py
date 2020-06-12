425b316c7f41aa4e02a6a92ae187f76ab25d0ab6
Binu <binua@mcmaster.ca>
2020-06-12 08:26:11 -0400

    Module that calls apis to get stock prices and send emails

---
+++ src/notify.py
@@ -0,0 +1,28 @@
+import ConfigParser
+from company import company
+from person import person
+import importlib
+sp = importlib.import_module("getStockPrice")
+
+class notify():
+    def __init__(self):
+        config = ConfigParser.ConfigParser()
+        config.read("../apiKeys.properties")
+        apiKey=config.get("financialmodelingprep", "notifyStocksApiKey")
+
+        companies=["ZNGA","DGLY","SNE"]
+
+        stockPrices={}
+
+        #testStockPrices={'DGLY': "5.37", 'SNE': "70.51", 'GOOGL': "1464.7"}
+
+        for comp in companies:
+            companyObj = company(comp)
+            url = companyObj.getUrl(apiKey)
+            stockPrice = sp.getStockPrice(url)
+            stockPrices[comp] = str(stockPrice)
+
+        binu = person("binua@mcmaster.ca","Stock Notifications",stockPrices)
+        binu.sendEmail()
+
+        #print(sp.getStockPrice(url))
